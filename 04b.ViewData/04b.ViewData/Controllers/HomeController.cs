a449b1e325576eddbf90d14e697af4ffc36e766b
asif7480 <a.a.khan7480@gmail.com>
2025-02-01 01:03:34 +0500

    Added view data

---
+++ 04b.ViewData/04b.ViewData/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04b.ViewData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["username"] = "abc";
+            ViewData["age"] = 22;
+            ViewData["canCode"] = true;
+            List<string> months = new List<string> { "Jan", "Feb", "Mar" };
+            ViewData["months"] = months;
+            return View();
+        }
+    }
+}
