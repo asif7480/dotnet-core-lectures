361b32697a43642988ce029e0d4a75a815ac6709
asif7480 <a.a.khan7480@gmail.com>
2025-01-31 23:06:54 +0500

    Added View Start lecture

---
+++ 03.ViewStart/03.ViewStart/Controllers/AdminController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _03.ViewStart.Controllers
+{
+	public class AdminController : Controller
+	{
+		public IActionResult AllUsers()
+		{
+			return View();
+		}
+	}
+}
