c7d095e6e8602df29dc7c86d9a773e1b4d891fdc
asif7480 <a.a.khan7480@gmail.com>
2025-02-01 00:34:49 +0500

    Added view bag

---
+++ 04a.ViewBag/04a.ViewBag/Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04a.ViewBag.Controllers
+{
+	public class HomeController : Controller
+	{
+		public IActionResult Index()
+		{
+			ViewBag.Username = "abc";
+			ViewBag.Age = 24;
+			ViewBag.canCode = true;
+			List<string>courses = new List<string> { "Php", "Python", "Java", "Nodejs" };
+			ViewBag.courses = courses;
+			return View();
+		}
+
+		public IActionResult SecondPage()
+		{
+			return View();
+		}
+
+		public IActionResult ThirdPage()
+		{
+			return View();
+		}
+	}
+}
