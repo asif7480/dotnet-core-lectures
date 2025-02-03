fb876c2ad79f309e31de5420022838c484bed579
asif7480 <a.a.khan7480@gmail.com>
2025-02-03 23:18:13 +0500

    Added temp data

---
+++ 04c.TempData/04c.TempData/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _04c.TempData.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.Val1 = "Value of view bag";     
+            ViewData["Val2"] = "I am view data";
+            TempData["Val3"] = "I am temp data";
+
+            //List<string> subjects = new List<string> { "html", "css", "js" };
+            //TempData["Val4"] = subjects;
+            TempData["Val4"] = new List<string>()
+            {
+                "html", "css", "js"
+            };
+            TempData.Keep("Val3");
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            TempData.Keep("Val3");
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            TempData.Keep("Val3");
+            return View();
+        }
+    }
+}
