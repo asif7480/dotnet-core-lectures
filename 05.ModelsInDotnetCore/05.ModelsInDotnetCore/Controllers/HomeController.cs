17c085ee37b637e641d3c5d9c2957709d3e8b797
asif7480 <a.a.khan7480@gmail.com>
2025-02-03 23:58:33 +0500

    added model

---
+++ 05.ModelsInDotnetCore/05.ModelsInDotnetCore/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using _05.ModelsInDotnetCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05.ModelsInDotnetCore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            var employees = new List<EmployeeModel>
+            {
+                new EmployeeModel{ empId= 1001, empName= "abc", dept= "hr", salary= 50000 },
+                new EmployeeModel{ empId= 1002, empName= "xyz", dept= "finance", salary= 45000 }
+            };
+
+            ViewData["employees"] = employees;
+
+            return View();
+        }
+    }
+}
---
+++ 05.ModelsInDotnetCore/05.ModelsInDotnetCore/Models/EmployeeModel.cs
@@ -0,0 +1,10 @@
+﻿namespace _05.ModelsInDotnetCore.Models
+{
+    public class EmployeeModel
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public string dept { get; set; }
+        public int salary { get; set; }
+    }
+}
