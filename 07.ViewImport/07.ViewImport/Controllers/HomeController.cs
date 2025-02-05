fa8510c29f9b72c144318e11bd9eed998bd8a6a9
asif7480 <a.a.khan7480@gmail.com>
2025-02-06 01:00:51 +0500

    Added view imports

---
+++ 07.ViewImport/07.ViewImport/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using _07.ViewImport.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _07.ViewImport.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Employee> employees = new List<Employee> 
+            {
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 40000 },
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 60000 }
+            };
+            return View(employees);
+        }
+
+        public IActionResult About()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 40000 },
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 60000 }
+            };
+            return View(employees);
+        }
+
+        public IActionResult Contact()
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 40000 },
+                new Employee { empId= 1001, empName= "abc", email= "abc@gmail.com", salary= 60000 }
+            };
+            return View(employees);
+        }
+    }
+}
---
+++ 07.ViewImport/07.ViewImport/Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace _07.ViewImport.Models
+{
+    public class Employee
+    {
+        public int empId { get; set; }
+        public string empName { get; set; }
+        public string email { get; set; }
+        public int salary { get; set; }
+    }
+}
