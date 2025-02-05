2fc36fd4376d0b547a5a8390cffab732e6f89eb4
asif7480 <a.a.khan7480@gmail.com>
2025-02-06 01:46:27 +0500

    Added tag helper but not working

---
+++ 08.TagHelpersPart1/08.TagHelpersPart1/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace _08.TagHelpersPart1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+    }
+}
