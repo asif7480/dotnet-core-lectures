61ad70ef58ff76053d0cd3137af5500cc0fa88b6
asif7480 <a.a.khan7480@gmail.com>
2025-02-08 01:33:50 +0500

    Added validation

---
+++ 10.TagHelperWithValidation/10.TagHelperWithValidation/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using _10.TagHelperWithValidation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _10.TagHelperWithValidation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee emp)
+        {
+            //return View();
+            return Content($"Name is: {emp.Name} and Email is: {emp.Email}");
+        }
+
+        public IActionResult AddRecord()
+        {
+            return View();
+        }
+    }
+}
