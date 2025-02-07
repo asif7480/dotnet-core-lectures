8b37e039aa4355a9aec9224b414dfb546a1238b4
asif7480 <a.a.khan7480@gmail.com>
2025-02-08 00:38:12 +0500

    Added tag helpers

---
+++ 09.TagHelpersPart2/09.TagHelpersPart2/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using _09.TagHelpersPart2.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _09.TagHelpersPart2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee emp)
+        {
+            return Content($"Name: {emp.EmpName}, Email: {emp.Email}, Department: {emp.Department}, Country: {emp.Country}, Gender: {emp.Gender}, Description: {emp.Description}");
+            
+        }
+
+        public IActionResult AddEmployee()
+        {
+            return View();
+        }
+    }
+}
---
+++ 09.TagHelpersPart2/09.TagHelpersPart2/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿namespace _09.TagHelpersPart2.Models
+{
+    public class Employee
+    {
+        public string EmpName { get; set; }
+        public string Email { get; set; }
+        public string Department { get; set; }
+        public Countries Country { get; set; }
+        public string Gender { get; set; }
+
+        public string Description { get; set; }
+    }
+
+    public enum Countries
+    {
+        Pakistan,
+        India,
+        Australia,
+        England
+    }
+}
