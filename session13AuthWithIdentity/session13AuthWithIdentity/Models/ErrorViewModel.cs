b691926285e52e9e6cbd09d010818d07a05666f8
asif7480 <a.a.khan7480@gmail.com>
2025-02-22 00:29:55 +0500

    Added authentication with identity

---
+++ session13AuthWithIdentity/session13AuthWithIdentity/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace session13AuthWithIdentity.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
