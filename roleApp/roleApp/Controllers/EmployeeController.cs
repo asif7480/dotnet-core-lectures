32531e995c7a527805c89f9293826ba81872ad4f
asif7480 <a.a.khan7480@gmail.com>
2025-03-13 06:49:37 +0500

    Added role based authentication

---
+++ roleApp/roleApp/Controllers/EmployeeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace roleApp.Controllers
+{
+    [Authorize(Roles = "Employee")]
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
